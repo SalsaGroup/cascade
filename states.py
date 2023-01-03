1fbbece1d1998a30ee2a90c2b195f31db2ce8821
Ryan Howell <rmhowell@protonmail.com>
2023-01-02 17:09:27 -0900

    initialize

---
+++ states.py
@@ -0,0 +1,20 @@
+import csv
+from csv import DictReader
+
+filename="states.csv"
+
+
+states={"State":[], "Abbr":[], "State Capital":[], "Region":[]};
+
+with open(filename, 'r') as data:
+    dict_reader = DictReader(data, dialect=csv.excel_tab)
+    list_of_dict = list(dict_reader)
+    
+
+for i in range(len(list_of_dict)):
+    print(list_of_dict[i])
+
+new_dict = {}
+for item in list_of_dict:
+    state = item.pop('State')
+    new_dict[state] = item
